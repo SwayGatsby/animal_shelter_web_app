846a0b9cff8f7b38c51284414b77995d8fa4a5e1
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-22 18:27:31 +0100

    added save method for customer class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require('pry')
 require_relative('../models/animal.rb')
+require_relative('../models/customer.rb')
+
 
 animal_1 = Animal.new({
   'name' => 'Agent Cooper',
@@ -8,10 +10,21 @@ animal_1 = Animal.new({
   'admission_date' => '2017-10-22',
   'adoptable' => true
   })
-  
+
 animal_1.save()
 
+customer_1 = Customer.new({
+  'first_name' => 'Kayla',
+  'last_name' => 'Fowler',
+  'phone' => '01315555555',
+  'email' => 'kayla@email.com',
+  'address_line_1' => 'Flat 404',
+  'address_line_2' => '30 Long Road',
+  'address_city' => 'Edinburgh',
+  'address_post_code' => 'EH1 1XX'
+  })
 
+  customer_1.save()
 
 binding.pry
 nil
---
+++ models/customer.rb
@@ -0,0 +1,41 @@
+require_relative('../db/sql_runner.rb')
+
+class Customer
+
+  attr_reader(:id, :first_name, :last_name, :phone, :email, :address_line_1, :address_line_2, :address_city, :address_post_code )
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @phone = options['phone']
+    @email = options['email']
+    @address_line_1 = options['address_line_1']
+    @address_line_2 = options['address_line_2']
+    @address_city = options['address_city']
+    @address_post_code = options['address_post_code']
+  end
+
+  def save()
+    sql = "INSERT INTO customers
+    (
+      first_name,
+      last_name,
+      phone,
+      email,
+      address_line_1,
+      address_line_2,
+      address_city,
+      address_post_code
+    )
+    VALUES
+    (
+      $1, $2, $3, $4, $5, $6, $7, $8
+    )
+    RETURNING id"
+    values = [@first_name, @last_name, @phone, @email, @address_line_1, @address_line_2, @address_city, @address_post_code]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i()
+  end
+
+end
