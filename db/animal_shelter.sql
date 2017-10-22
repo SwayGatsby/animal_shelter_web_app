497647cbc14d3bdf0b9259d5af0b6b636e18a3c7
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-22 16:46:48 +0100

    added SQL runner and SQL file defining tables

---
+++ db/animal_shelter.sql
@@ -0,0 +1,36 @@
+DROP TABLE adoptions;
+DROP TABLE customer;
+DROP TABLE animals;
+
+CREATE TABLE customers
+(
+  id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  phone VARCHAR(20) NOT NULL,
+  email VARCHAR(255),
+  address_line_1 VARCHAR(255) NOT NULL,
+  address_line_2 VARCHAR(255),
+  address_city VARCHAR(255) NOT NULL,
+  address_post_code VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE animals
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  breed VARCHAR(255),
+  admission_date DATE NOT NULL,
+  adoptable BOOLEAN NOT NULL
+);
+
+
+CREATE TABLE adoptions
+(
+  id SERIAL8 PRIMARY KEY,
+  customer_id INT8 REFERENCES customers(id),
+  animal_id INT8 REFERENCES animals(id),
+  date_adopted DATE,
+  checkup_date DATE
+);
