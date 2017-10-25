21e6d4d8a3acd37bf36265cc2969edf4ec47670e
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-25 21:23:12 +0100

    added some css

--- controllers/customers_controller.rb
+++ controllers/customers_controller.rb
@@ -6,3 +6,20 @@ get '/customers' do
   @customers = Customer.all()
   erb(:"customers/index")
 end
+
+get '/customers/new' do
+  erb(:"customers/new")
+end
+
+post '/customers' do
+  @customer = Customer.new(params)
+  @customer.save()
+  redirect to '/customers'
+end
+
+
+#
+# get '/customers/:id/edit' do
+#   @customer = Customer.find(params[:id])
+#   erb(:"customers/edit")
+# end
